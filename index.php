5055d4160c89a926afdb0b281517c2176e5e88b4
Max Lee <maxwell.a.lee56@gmail.com>
2019-04-15 16:16:28 -0400

    Made a home page

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ $f3 = Base::instance();
 
 //define a default route
 $f3->route('GET /', function () {
-    echo '<h1>Hello World!</h1>';
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    //echo '<h1>Hello World!</h1>';
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run fat-free
